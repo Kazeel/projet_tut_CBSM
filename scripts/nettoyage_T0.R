f95ce0e9f352a8e345637b41cbf9a6da28382eb5
Kazeel <kazeel@hotmail.fr>
2019-10-23 17:30:02 +0200

    avancement de nettoyage T0

--- scripts/nettoyage_T0.R
+++ scripts/nettoyage_T0.R
@@ -11,3 +11,26 @@ physio_T0<-data_physio[,5:21]
 
 # étape 2 : retirons les individus n'ayant aucun relevé
 physio_T0<-physio_T0[apply(is.na(physio_T0),1,sum)==0,]
+
+# étape 3 : cherchons les individus trop éloignés de la norme groupe.
+
+## on va créer une fonction
+# on y rentre un vecteur de données
+# souvent on considère que ce qui s'écarte de 2 écart-type de la moyenne n'est pas normal
+# mais on va laisser le choix de la taille de cet intervalle (2 ou 3 sigma ?)
+cherche.ext<-function(x,taille=2){
+  sigma<-sd(x) # pour recalculer plusieurs fois l'écart type
+  ext<-mean(x)+c(-taille*sigma,2*sigma) # 2 valeurs de l'intervalle
+  result<-(x>=ext[1])&(x<=ext[2]) # TRUE si ok, FALSE si extrême
+  return(result)
+}
+
+# on applique la fonction à nos données et on va conserver un vecteur qui indique si un individu est sans problème
+
+T0_indi_sp<-apply(apply(physio_T0,2, cherche.ext),1,sum)==17
+
+# on peut regarder combien d'individus sont sans problème
+
+sum(T0_indi_sp)
+
+# Maintenant on peut chercher une solution pour prédire les valeurs des personnes avec des problèmes !
