64e16de30855e6f71031edaa58ba35a19c90f762
Kazeel <kazeel@hotmail.fr>
2019-11-05 16:14:42 +0100

    placement des fonctions crées dans packages

--- scripts/nettoyage_T1.R
+++ scripts/nettoyage_T1.R
@@ -7,23 +7,23 @@
 ## nous n'avons pas besoin des identifiants ou dur groupe pour les valeurs de T0
 ## en effet, T0 se situe avant expérience, il n'y a donc pas de différence entre les individus.
 ## Ils proviennent tous de la même population de départ.
-physio_T0<-data_physio[,5:21]
+physio_T1<-data_physio[,c(4,22:38)]
 
 # étape 2 : retirons les individus n'ayant aucun relevé
-physio_T0<-physio_T0[apply(is.na(physio_T0),1,sum)==0,]
+physio_T1<-physio_T1[apply(is.na(physio_T1),1,sum)==0,]
 
 # étape 3 : cherchons les individus trop éloignés de la norme groupe.
 
 ## on va utiliser la fonction cherche.ext
 
-
-
 # on applique la fonction à nos données et on va conserver un vecteur qui indique si un individu est sans problème
 
-T0_indi_sp<-apply(apply(physio_T0,2, cherche.ext),1,sum)==17
+T1_indi_sp<-apply(apply(physio_T1[,-1],2, cherche.ext),1,sum)==17
 
 # on peut regarder combien d'individus sont sans problème
 
-sum(T0_indi_sp)
+sum(T1_indi_sp)
 
 # Maintenant on peut chercher une solution pour prédire les valeurs des personnes avec des problèmes !
+
+## Nous allons faire de la validation croisée, avec un ratio 3/4 - 1/4.
--- scripts/packages.R
+++ scripts/packages.R
@@ -2,3 +2,17 @@
 
 #Importation
 library(readxl)
+
+# fonctions utiles
+
+
+cherche.ext<-function(x,taille=2){
+  sigma<-sd(x) # pour recalculer plusieurs fois l'écart type
+  ext<-mean(x)+c(-taille*sigma,2*sigma) # 2 valeurs de l'intervalle
+  result<-(x>=ext[1])&(x<=ext[2]) # TRUE si ok, FALSE si extrême
+  return(result)
+  
+  # on y rentre un vecteur de données
+  # souvent on considère que ce qui s'écarte de 2 écart-type de la moyenne n'est pas normal
+  # mais on va laisser le choix de la taille de cet intervalle (2 ou 3 sigma ?)
+}
